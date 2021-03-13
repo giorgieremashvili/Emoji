cf5fefc3ac43d458b6daa5029b093a83f11232cb
giorgieremashvili <eremashvili.g123@gmail.com>
2021-03-13 17:25:19 +0400

    emojidavaleba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import emojiList from'./emojiList.json';
+
 
 function App() {
+  const compareValue = (value) => {
+
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <header classname="title-header">
+  Emoji Search
+</header>
+<div className="emoji-search"><input placeholder="search" onChange={(e) => compareValue(e.target.value)}></input></div>
+{ emojiList.map(item => {
+  return(
+    <div className="emoji">
+      <span class="info">Click To Copy Emoji</span>
+  <ul>
+    <li>{item.symbol} {item.title}</li>
+  </ul>
+</div>
+  )
+  
+})}
+
+</div>
+      
   );
+  
 }
 
 export default App;
