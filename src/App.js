e0f99728285586c85a855cef856166661dcb5af5
alex <aleqsi.tsiklauri22@gmail.com>
2021-03-16 20:24:12 +0400

    Copy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import emojiList from'./emojiList.json';
-
+import copy from 'copy-to-clipboard';
 
 function App() {
-  const compareValue = (value) => {
-
+  const copyToClipBoard = (value) => {
+    console.log(value);
+    copy(value);
   }
-
+ const SearchEmoji = (searchvalue) => {
+   
+ }
   return (
     
     <div className="App">
       <header classname="title-header">
   Emoji Search
 </header>
-<div className="emoji-search"><input placeholder="search" onChange={(e) => compareValue(e.target.value)}></input></div>
+<div className="emoji-search"><input placeholder="search" onKeyPress={(e) => SearchEmoji(e.target.value)} ></input></div>
 { emojiList.map(item => {
   return(
-    <div className="emoji">
-      <span class="info">Click To Copy Emoji</span>
+    <div   className="emoji">
+      
+      <span  onClick={() => copyToClipBoard(item.symbol)} class="info">Click To Copy Emoji</span>
   <ul>
     <li>{item.symbol} {item.title}</li>
   </ul>
@@ -32,5 +36,4 @@ function App() {
   );
   
 }
-
 export default App;
